0aab8c4b31d8553304f2a428faba03d78b86c5ea
DESKTOP-186F7I7\vpara <vinpar1997@gmail.com>
2017-05-05 08:41:42 -0400

    truc

--- Hockey/app/Http/Controllers/SaisonController.php
+++ Hockey/app/Http/Controllers/SaisonController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use Validator;
 use Illuminate\Http\Request;
+use App\Joueur;
+use App\Equipe;
+use Dingo\Api\Routing\Helpers;
+use Illuminate\View\View;
 
 class SaisonController extends Controller
 {
-    //
+	use Helpers;
+	
+    public function index() : View
+    {
+        $saisons = $this->api->get('/raw/saisons/');
+        $saisons = Saison::All();
+        return view('saisons.index', compact('saisons'));
+    }
 }
